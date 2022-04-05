d979c40beaf5cf2cbec0d0cac456ed9028bdbb7f
Gabrijela Srbalj <gabrijela.srbalj@gmail.com>
2022-04-05 08:21:46 +0200

    Doctor Patient Model

---
+++ Hospital/Hospital.Model/DoctorPatientModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Model
+{
+    public class DoctorPatientModel
+    {
+        public Guid PatientID { get; set; }
+
+        public  Guid DoctorID { get; set; }
+
+        public int DoctorPatientID { get; set; }
+    }
+}
